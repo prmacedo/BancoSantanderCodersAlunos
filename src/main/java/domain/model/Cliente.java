acd6e430bd0a7f8a97c94a590b9dee10d05b37fc
Matheus Borges <matheusborgesmoraes1@gmail.com>
2024-05-14 02:31:41 -0300

    Add H2 to project

--- src/main/java/domain/model/Cliente.java
+++ src/main/java/domain/model/Cliente.java
@@ -1,12 +1,23 @@
 package domain.model;
 
 public class Cliente {
+    private String id;
     private String nome;
     private String cpf;
 
     public Cliente(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
+    }
+
+    public Cliente(String id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getNome() {
